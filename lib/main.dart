c62fa9dc6e3f10409d5d3ae83132a6362acf6d4c
Mert Oğuzhan <55846968+karasword55@users.noreply.github.com>
2022-02-04 22:41:01 +0300

    Update main.dart
    firebaseOptionwsChanged

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,7 @@ import 'package:mert_oguzhan/debug/MyAppForMert.dart';
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: FirebaseOptions(apiKey: 'AIzaSyDD3Ab_WmWHYqUVi7OmWG8rYpf9pjON_PY', 
-  appId: '1:62753469944:android:87f37b0d55a4a0edf9120d',
-  messagingSenderId: '62753469944', projectId: 'mertoguzhan-5d2de'));
+  await Firebase.initializeApp();
 
   String kullanici="mert";
 
